3113358b77a61a3287e5a0934b9983fe10559ef8
David Reeves <djreeve693@pccsk12.com>
2022-07-01 00:17:27 -0400

    get more layout done

--- md.py
+++ md.py
@@ -8,6 +8,18 @@ KVContents = open('Login.kv', encoding='utf8').read()
 class Login(FloatLayout):
     def rgba255to1(self, rgba):
         return (rgba[0]/255, rgba[1]/255, rgba[2]/255, rgba[3])
+    
+    def submitForm(self):
+        print("Submitted form!")
+        v = True
+        if(self.ids.username.text == ""):
+            v = False
+            
+            self.ids.username.md_bg_color = self.rgba255to1((50, 0, 0, 1))
+        if(self.ids.password.text == ""):
+            v = False
+            self.ids.password.md_bg_color = self.rgba255to1((50, 0, 0, 1))
+        
 
 class LoginPage(MDApp):
     def __init__(self):
@@ -16,6 +28,7 @@ class LoginPage(MDApp):
         
     def build(self):
         return self.screen
+    
     
     
     
