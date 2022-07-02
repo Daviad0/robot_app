dee144b3e50616b9852f19265f8961cd2c10674e
David Reeves <djreeve693@pccsk12.com>
2022-07-02 12:12:30 -0400

    create base actions class with SparkClub

--- actions.py
+++ actions.py
@@ -1,4 +1,5 @@
 from kivy.network.urlrequest import UrlRequest
+import requests
 
 class SparkClub():
     def __init__(self):
@@ -15,9 +16,24 @@ class SparkClub():
             password: password,
             group: "testing-env" # change to the lightning robotics group name later
         })
-        pass
+        if(res.status == 200 and data.successful):
+            self.account.loggedIn = True
+            self.account.token = data.token
+            self.account.permissions = data.permissions
+            self.account.username = data.username
+            self.account.email = data.email
+            print("Logged in!")
+            return True
+        else:
+            return False
     
     def _sendAPIRequest(self, method, endpoint, data=None):
-        pass
+        cookies = {}
+        if(self.account.loggedIn):
+            cookies["token"] = self.account.token
+        res = requests.request(method, "http://localhost:8080" + endpoint, data=data, cookies=cookies)
+        data = res.json()
+        
+        return {status: res.status_code, data: data}
         
     
