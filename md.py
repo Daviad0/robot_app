35abaf3811d7cba3e26bf9054ccd76384d947dfa
David Reeves <djreeve693@pccsk12.com>
2022-07-05 23:20:57 -0400

    add more interaction and functions that are overall useful

--- md.py
+++ md.py
@@ -2,24 +2,74 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.animation import Animation
 from actions import SparkClub
 
 KVContents = open('Login.kv', encoding='utf8').read()
 
 SCI = SparkClub()
 
+def rgba255to1(rgba):
+    return (rgba[0]/255, rgba[1]/255, rgba[2]/255, rgba[3])
 
+def fadeto(widget, opacity, duration):
+    a = Animation(opacity=opacity, duration=duration)
+    a.start(widget)
+
+COLORS = {
+    "primary": rgba255to1((19, 3, 252,1)),
+    "white": rgba255to1((255, 255, 255,1)),
+    "gray" : rgba255to1((100, 100, 100,1)),
+}
 
 
 class Login(FloatLayout):
-    def rgba255to1(self, rgba):
-        return (rgba[0]/255, rgba[1]/255, rgba[2]/255, rgba[3])
+   
     
     def submitForm(self):
         print("Submitted form!")
         v = True
         res = SCI.login(self.ids.username.text, self.ids.password.text)
         print(res)
+    def getColor(self, name):
+        return COLORS[name.lower()]
+    def goToTempLogin(self):
+        print("Go to temp login")
+        self.ids.login_page.pos_hint = {'center_x': 0.5, 'center_y': 10}
+        self.ids.temp_page.pos_hint = {'center_x': 0.5, 'center_y': 0.75}
+    def usernameChange(self):
+        if(self.ids.username.text == ""):
+            # keep password disabled
+            self.ids.password.disabled = True
+            
+            fadeto(self.ids.password, 0.3, 0.25)
+            self.ids.login_button.disabled = True
+            fadeto(self.ids.login_button, 0.3, 0.25)
+        else:
+            self.ids.password.disabled = False
+            fadeto(self.ids.password, 1, 0.25)
+    def passwordChange(self):
+        if(self.ids.username.text == "" or self.ids.password.text == ""):
+            # disable login button
+            self.ids.login_button.disabled = True
+            fadeto(self.ids.login_button, 0.3, 0.25)
+        else:
+            self.ids.login_button.disabled = False
+            fadeto(self.ids.login_button, 1, 0.25)
+    def emailChange(self):
+        if(self.ids.temp_email.text == "" or not "@" in self.ids.temp_email.text):    
+            self.ids.temp_send_code.disabled = True
+            fadeto(self.ids.temp_send_code, 0.3, 0.25)
+        else:
+            self.ids.temp_send_code.disabled = False
+            fadeto(self.ids.temp_send_code, 1, 0.25)
+    def sendCode(self):
+        # do the temp code passing here
+        if(self.ids.temp_email.text == ""):
+            return
+        self.ids.temp_code.disabled = False
+        fadeto(self.ids.temp_code, 1, 0.25)
+            
     
 
 class LoginPage(MDApp):
@@ -28,7 +78,9 @@ class LoginPage(MDApp):
         self.screen = Builder.load_string(KVContents)
         
     def build(self):
+        
         return self.screen
+    
     
     
     
