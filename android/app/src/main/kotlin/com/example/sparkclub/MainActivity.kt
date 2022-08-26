d4b5252c0d6ecb9c526f982bbeaf4be2e44ce979
WindowsVistaisCool <kyle@quackings.com>
2022-08-26 18:46:45 -0400

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/sparkclub/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sparkclub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SparkClub',
+      theme: ThemeData.light().copyWith(
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF0056d2)),
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SparkClub'),
+      ),
+    );
+  }
+}
